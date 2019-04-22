7bccf21ef2617b93059611daae72fda240a5de6e
Anton Bohomaz <aboho@softserveinc.com>
2019-04-22 12:03:06 +0300

    Add not found error handling.

--- HdProduction.HelpDesk.Infrastructure/Services/TicketStatusService.cs
+++ HdProduction.HelpDesk.Infrastructure/Services/TicketStatusService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using HdProduction.HelpDesk.Domain.Contract;
 using HdProduction.HelpDesk.Domain.Entities;
+using HdProduction.HelpDesk.Domain.Exceptions;
 
 namespace HdProduction.HelpDesk.Infrastructure.Services
 {
@@ -23,7 +24,7 @@ namespace HdProduction.HelpDesk.Infrastructure.Services
 
         public async Task<TicketStatus> UpdateAsync(long id, TicketStatus ticketStatus)
         {
-            var statusEntity = await _ticketStatusRepository.FindAsync(id);
+            var statusEntity = await FindById(id);
             statusEntity.Name = ticketStatus.Name;
             await _ticketStatusRepository.SaveAsync();
             return statusEntity;
@@ -36,12 +37,13 @@ namespace HdProduction.HelpDesk.Infrastructure.Services
         
         public async Task<TicketStatus> FindById(long id)
         {
-            return await _ticketStatusRepository.FindAsync(id);
+            return await _ticketStatusRepository.FindAsync(id)
+                   ?? throw new EntityNotFoundException("Ticket status doesn't exist");;
         }
 
         public async Task DeleteAsync(long id)
         {
-            var ticketStatus = await _ticketStatusRepository.FindAsync(id);
+            var ticketStatus = await FindById(id);
             _ticketStatusRepository.Remove(ticketStatus);
             await _ticketStatusRepository.SaveAsync();
         }
